3d8a10edc74c823b47ddc7665ad46f1679000fe3
Garik Asatryan <asa7ur@proton.me>
2025-12-23 20:54:18 +0100

    Initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,149 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS performances;
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS sponsor_stage;
+DROP TABLE IF EXISTS stages;
+DROP TABLE IF EXISTS sponsors;
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS ticket_types;
+DROP TABLE IF EXISTS campings;
+DROP TABLE IF EXISTS camping_types;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS users;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- 1. USUARIOS Y SEGURIDAD
+CREATE TABLE users
+(
+    id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email        VARCHAR(100) UNIQUE NOT NULL,
+    password     VARCHAR(100)        NOT NULL,
+    enabled      BOOLEAN             NOT NULL,
+    first_name   VARCHAR(50)         NOT NULL,
+    last_name    VARCHAR(50)         NOT NULL,
+    phone        VARCHAR(20),
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE roles
+(
+    id   BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE user_role
+(
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
+
+-- 2. VENTAS Y COMPRAS
+CREATE TABLE orders
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    order_date  TIMESTAMP                             DEFAULT CURRENT_TIMESTAMP,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status      ENUM ('PENDING', 'PAID', 'CANCELLED') DEFAULT 'PENDING',
+    user_id     BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE ticket_types
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(50)    NOT NULL,
+    price           DECIMAL(10, 2) NOT NULL,
+    stock_total     INT            NOT NULL,
+    stock_available INT            NOT NULL
+);
+
+CREATE TABLE tickets
+(
+    id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    attendee_name            VARCHAR(100)                    NOT NULL,
+    attendee_last_name       VARCHAR(100)                    NOT NULL,
+    attendee_document_type   ENUM ('DNI', 'NIE', 'PASSPORT') NOT NULL,
+    attendee_document_number VARCHAR(20)                     NOT NULL,
+    attendee_birth_date      DATE                            NOT NULL,
+    qr_code                  VARCHAR(255) UNIQUE,
+    status                   ENUM ('ACTIVE', 'USED', 'CANCELLED') DEFAULT 'ACTIVE',
+    ticket_type_id           BIGINT,
+    order_id                 BIGINT,
+    FOREIGN KEY (ticket_type_id) REFERENCES ticket_types (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id)
+);
+
+CREATE TABLE camping_types
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(50)    NOT NULL,
+    price           DECIMAL(10, 2) NOT NULL,
+    stock_total     INT            NOT NULL,
+    stock_available INT            NOT NULL
+);
+
+CREATE TABLE campings
+(
+    id                       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    attendee_name            VARCHAR(100) NOT NULL,
+    attendee_document_number VARCHAR(20)  NOT NULL,
+    qr_code                  VARCHAR(255) UNIQUE,
+    status                   ENUM ('ACTIVE', 'USED', 'CANCELLED') DEFAULT 'ACTIVE',
+    camping_type_id          BIGINT,
+    order_id                 BIGINT,
+    FOREIGN KEY (camping_type_id) REFERENCES camping_types (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id)
+);
+
+-- 3. LOGÍSTICA DEL FESTIVAL
+CREATE TABLE sponsors
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(100),
+    phone        VARCHAR(20),
+    email        VARCHAR(100),
+    contribution DECIMAL(10, 2),
+    image        VARCHAR(255)
+);
+
+CREATE TABLE stages
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(100),
+    capacity BIGINT
+);
+
+CREATE TABLE sponsor_stage
+(
+    sponsor_id BIGINT,
+    stage_id   BIGINT,
+    PRIMARY KEY (sponsor_id, stage_id),
+    FOREIGN KEY (sponsor_id) REFERENCES sponsors (id),
+    FOREIGN KEY (stage_id) REFERENCES stages (id)
+);
+
+CREATE TABLE artists
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(100),
+    phone   VARCHAR(20),
+    email   VARCHAR(100),
+    genre   VARCHAR(100),
+    country VARCHAR(100)
+);
+
+CREATE TABLE performances
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    start_time DATETIME NOT NULL,
+    end_time   DATETIME NOT NULL,
+    artist_id  BIGINT   NOT NULL,
+    stage_id   BIGINT   NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists (id),
+    FOREIGN KEY (stage_id) REFERENCES stages (id)
+);
---
+++ src/test/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/ValkyrfestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.GarikAsatryan.valkyrfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValkyrfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
