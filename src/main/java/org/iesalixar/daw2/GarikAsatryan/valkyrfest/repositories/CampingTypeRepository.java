a3f6c1cb43ec369f92a1bd820b11444017dec05a
Garik Asatryan <asa7ur@proton.me>
2025-12-25 18:39:26 +0100

    Added repos

---
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/repositories/CampingTypeRepository.java
@@ -0,0 +1,7 @@
+package org.iesalixar.daw2.GarikAsatryan.valkyrfest.repositories;
+
+import org.iesalixar.daw2.GarikAsatryan.valkyrfest.entities.CampingType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampingTypeRepository extends JpaRepository<CampingType, Long> {
+}
---
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/repositories/TiciketTypeRepository.java
@@ -0,0 +1,7 @@
+package org.iesalixar.daw2.GarikAsatryan.valkyrfest.repositories;
+
+import org.iesalixar.daw2.GarikAsatryan.valkyrfest.entities.TicketType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TiciketTypeRepository extends JpaRepository<TicketType, Long> {
+}
