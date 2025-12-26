783514ed6ef55bce3b6866098a4a8a223032d98c
Garik Asatryan <asa7ur@proton.me>
2025-12-26 12:32:33 +0100

    Minor changes

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/dto/CampingRequestDTO.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/dto/CampingRequestDTO.java
@@ -16,30 +16,24 @@ import java.time.LocalDate;
 public class CampingRequestDTO {
     @NotBlank(message = "{msg.validation.required}")
     @Size(max = 100, message = "{msg.validation.size}")
-    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
 
     @NotBlank(message = "{msg.validation.required}")
     @Size(max = 100, message = "{msg.validation.size}")
-    @Column(name = "last_name", nullable = false, length = 100)
     private String lastName;
 
     @NotNull(message = "{msg.validation.required}")
     @Enumerated(EnumType.STRING)
-    @Column(name = "document_type", nullable = false)
     private DocumentType documentType;
 
     @NotBlank(message = "{msg.validation.required}")
     @Size(max = 20, message = "{msg.validation.size}")
-    @Column(name = "document_number", nullable = false, length = 20)
     private String documentNumber;
 
     @NotNull(message = "{msg.validation.required}")
     @IsAdult
-    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     @NotNull(message = "{msg.validation.required}")
-    @Column(name = "camping_type_id", nullable = false)
     private Long campingTypeId;
 }
--- src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/dto/UserRegistrationDTO.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest/dto/UserRegistrationDTO.java
@@ -13,30 +13,24 @@ public class UserRegistrationDTO {
     @NotBlank(message = "{msg.validation.required}")
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "{msg.validation.email}")
     @Size(max = 100, message = "{msg.validation.size}")
-    @Column(name = "email", unique = true, nullable = false, length = 100)
     private String email;
 
     @NotBlank(message = "{msg.validation.required}")
-    @Column(name = "password", nullable = false)
     private String password;
 
     @NotEmpty(message = "{msg.validation.required}")
     @Size(max = 100, message = "{msg.validation.size}")
-    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
 
     @NotEmpty(message = "{msg.validation.required}")
     @Size(max = 100, message = "{msg.validation.size}")
-    @Column(name = "last_name", nullable = false, length = 100)
     private String lastName;
 
     @NotNull(message = "{msg.validation.required}")
     @IsAdult
-    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     @NotEmpty(message = "{msg.validation.required}")
     @Size(max = 30, message = "{msg.validation.size}")
-    @Column(name = "phone", nullable = false, length = 30)
     private String phone;
 }
